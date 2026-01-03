d53fe550236124e06ddc62a1f84eeb3914e5aad6
Mr Eggleton <meggleton@utcsheffield.org.uk>
2026-01-03 11:37:49 +0000

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Digital Art Club",
+    pageTitle: "MakerClub",
     pageTitleSuffix: "UTC Sheffield OLP",
     enableSPA: true,
     enablePopovers: true,
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "digital-art-club..netlify.app",
+    baseUrl: "makerclub.netlify.app",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     generateSocialImages: false,
